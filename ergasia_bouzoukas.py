c3e337951cd03a93b2e0d476778d2711d0a1069b
bouzoukasz <zmpouzoukas@gmail.com>
2025-11-14 08:58:27 +0200

    Initial upload of homework 1

---
+++ ergasia_bouzoukas.py
@@ -0,0 +1,12 @@
+score = 40
+
+if score >= 85:
+    print("Grade: A - Excellent!")
+elif score >= 75:
+    print("Grade: B - Great job!")  
+elif score >= 70:
+    print("Grade: C - Good work")   
+elif score >= 60:
+    print("Grade: D - Passing")    
+else:
+    print("Grade: F - Need improvement")  
